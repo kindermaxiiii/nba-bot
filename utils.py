4b514421237fecbde027e5a28896a98b9f9f747d
kindermaxiiii <noahhauwel1@gmail.com>
2026-03-04 18:29:52 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import json
 import os
+import math
 
 
 def load_json(path, default=None):
@@ -17,3 +18,14 @@ def load_json(path, default=None):
 def save_json(path, data):
     with open(path, "w") as f:
         json.dump(data, f, indent=2)
+
+
+def clamp(x, lo, hi):
+    return max(lo, min(hi, x))
+
+
+def phi(x):
+    """
+    Standard normal CDF
+    """
+    return 0.5 * (1 + math.erf(x / math.sqrt(2)))
